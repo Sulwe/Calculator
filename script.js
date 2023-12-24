445d6c7e46e07591b5a711eb498d1f2977d537ff
Sulwe <cypriansulwe.cs@gmail.com>
2023-12-24 14:23:08 +0300

    remove parseInt from subtract fn add multiply and divide fns

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ function add(x, y){
     return parseInt(x) + parseInt(y);
 };
 
-let subtract = (x, y) => parseInt(x) - parseInt(y);
+let subtract = (x, y) => x - y;
+
+function multiply(x, y){
+    return x * y;
+};
+
+function divide(x,y){
+    return x / y;
+};
